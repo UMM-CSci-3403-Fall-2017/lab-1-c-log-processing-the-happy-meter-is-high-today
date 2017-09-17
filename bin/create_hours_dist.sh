a861ff679b17e8f20dec57849c9bce2bfc0de029
mitch723 <mitch723@morris,umn.edu>
2017-09-16 23:06:43 -0500

    Added create_hours_dist.sh into bin, and cleaned up create_username_dist.sh a bit since there was some code that was # out.

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+#!/bin/bash
+
 #Since this dosen't have to move anywhere in the directory its pretty stright forward.
 
-#Looking for a file called "failed_login_data.txt" from a list thats in the current directory
+
 cat $(find $1 -type f| grep "failed_login_data.txt")|
+#Looking for a file
+#cat "failed_login_data.txt"|
 #Prins the #nth from the begining
-awk '{print $4}'|
+awk '{print $3}'|
 #Sortig the data and filering the results into an uniq
 sort | uniq -c |
 #Take the filter from all the aove and prints out a new line everytime, into a temp.txt 
 awk '{print "data.addRow([\x27"$2"\x27, " $1"]);";}' > temp.txt
 
-./bin/wrap_contents.sh temp.txt html_components/username_dist $1/username_dist.html
+./bin/wrap_contents.sh temp.txt html_components/hours_dist $1/hours_dist.html
 
 #Cleaning to be nice
 rm temp.txt
