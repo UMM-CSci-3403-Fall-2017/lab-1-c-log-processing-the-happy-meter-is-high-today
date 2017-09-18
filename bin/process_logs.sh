3dd442a66d016c832d50d75525b7ca6f3bce23fa
mitch723 <mitch723@morris,umn.edu>
2017-09-17 22:28:47 -0500

    Working on the PROCESSING LOGS I think it some how masses up the zip file. Going to push and rebranch

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 #Creates a TempDir inorder to do work
-mkdir TempDir
 #saves the home dir to place the work
-home=$(pwd)
 
 #loop statement, *. is a loop with $1 takes the target files, $f is the call back that is assigned Lis.
-for f in *.$1
+#for f in *.$1
+#do
+#Lis=$(basename "$f" .tgz)
+#mkdir TempDir/$Ldis
+#tar -xzf $f -C TempDir/$Lis
+#bin/process_client_logs.sh TempDir/$Lis
+#done
+
+#bin/create_username_dist.sh TempDir
+#bin/create_hours_dist.sh TempDir
+#bin/create_country_dist.sh TempDir
+#bin/assemble_report.sh TempDir
+#mv failed_login_summary.html $home
+#rm -rf ThisDir
+
+
+mkdir Temp
+
+for f in "$@"
 do
-Lis=$(basename "$f" .tgz)
-mkdir TempDir/$Ldis
-tar -xzf $f -C TempDir/$Lis
-bin/process_client_logs.sh TempDir/$Lis
+	tarDir=$(basename "$f" .tgz)
+	mkdir Temp/$tarDir
+	tar -xzf $f -C Temp/$tarDir
+	bin/process_client_logs.sh Temp/$tarDir
 done
 
-bin/create_username_dist.sh TempDir
-bin/create_hours_dist.sh TempDir
-bin/create_country_dist.sh TempDir
-bin/assemble_report.sh TempDir
+bin/create_username_dist.sh Temp
 
-mv failed_login_summary.html $home
+bin/create_hours_dist.sh Temp
 
-rm -rf TempDir
+bin/create_country_dist.sh Temp
+
+bin/assemble_report.sh Temp
+
+here=$(pwd)
+
+mv Temp/failed_login_summary.html $here
+
+rm -rf Temp
