b415c61b457e11d1bbdc9f48839c97e068c49a66
Matt M <mitch723@morris.umn.edu>
2017-09-17 22:49:52 -0500

    Process_logs.sh works now

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 #Creates a TempDir inorder to do work
-mkdir TempDir
-#saves the home dir to place the work
-home=$(pwd)
-
-#loop statement, *. is a loop with $1 takes the target files, $f is the call back that is assigned Lis.
-for f in *.$1
+mkdir stemp
+#Tried to used *.$1 but I couldnt seem to get it to work in this case. In sead useing $@ to do the same thing.
+for f in "$@"
 do
-Lis=$(basename "$f" .tgz)
-mkdir TempDir/$Ldis
-tar -xzf $f -C TempDir/$Lis
-bin/process_client_logs.sh TempDir/$Lis
+holder=$(basename "$f" .tgz)
+mkdir stemp/$holder
+tar -xzf $f -C stemp/$holder
+bin/process_client_logs.sh stemp/$holder
 done
+#I think I might be able to make this into an array but not to sure how
+bin/create_username_dist.sh stemp
+bin/create_hours_dist.sh stemp
+bin/create_country_dist.sh stemp
+bin/assemble_report.sh stemp
 
-bin/create_username_dist.sh TempDir
-bin/create_hours_dist.sh TempDir
-bin/create_country_dist.sh TempDir
-bin/assemble_report.sh TempDir
-
-mv failed_login_summary.html $home
-
-rm -rf TempDir
+main=$(pwd)
+mv stemp/failed_login_summary.html $main
+#clean up is nice
+rm -rf stemp
